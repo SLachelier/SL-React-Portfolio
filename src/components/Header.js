f1901e81ddbc09d7f8ab0c32c9a07cbf12188dce
Shauna Lachelier <70386413+SLachelier@users.noreply.github.com>
2022-09-17 14:25:54 -0500

    added CSS and JSX for header and bg

---
+++ src/components/Header.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+const Header = () => {
+  return(
+    <header>
+        <div class="header">
+            <img src="./assets/images/logo.svg" alt="X" class="logo"/>
+        </div>
+        <button class="mobile-nav-toggle" aria-controls="primary-navigation" aria-expanded="false"><span class="sr-only">Menu</span></button>
+        <nav>
+            <ul data-visible="false" id="primary-navigation" class="primary-navigation flex">
+                <li class="active">
+                    <a class=" uppercase text-white letter-spacing-2" href="index.html">
+                        <span aria-hidden="true">[</span> Home <span aria-hidden="true">]</span>
+                    </a>
+                </li>
+                <li>
+                    <a class=" uppercase text-white letter-spacing-2" href="./pages/resume.html" target="_blank" rel="noreferrer">
+                        <span aria-hidden="true">[</span> Resume <span aria-hidden="true">]</span>
+                    </a>
+                </li>
+                <li>
+                    <a class=" uppercase text-white letter-spacing-2" href="https://github.com/SLachelier" target="_blank" rel="noreferrer">
+                        <span aria-hidden="true">[</span> GitHub <span aria-hidden="true">]</span>
+                    </a>
+                </li>
+                <li>
+                    <a class=" uppercase text-white letter-spacing-2" href="www.linkedin.com/in/slachelier/" target="_blank" rel="noreferrer">
+                        <span aria-hidden="true">[</span> LinkedIn <span aria-hidden="true">]</span>
+                    </a>
+                </li>
+                <li>
+                    <a class=" uppercase text-white letter-spacing-2" href="./pages/contact.html" target="_blank" rel="noreferrer">
+                        <span aria-hidden="true">[</span> Contact <span aria-hidden="true">]</span>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+
+  );
+};
+
+export default Header;
