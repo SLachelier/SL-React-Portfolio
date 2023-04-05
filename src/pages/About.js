2e8baf2ca522db8b57db009a8c99af21e16f6939
Shauna L <slachelier@gmail.com>
2023-04-05 14:44:55 -0500

    minor adjustments to bio and projects header

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,24 +1,45 @@
 function About() {
   return (
     <article>
-            <h2 className="title">About Me</h2>
-            <img id="photo" src="../../images/pfp.jpg" alt="placeholder" width="250" height="250"/>
-                <p>
-                I am a Fullstack Developer with a background in technical support engineering.  I recently earned a certificate in Full Stack Development from the University of Texas at Austin, where I developed skills in TypeScript, Sass, React.js, MongoDB and more. 
-                </p>
-                <p>
-                An innovative problem solver passionate about developing new technologies, with a focus on mobile-first practices and test-driven development. My schooling in 3D modeling and animation have prepared me to approach problems with creativity and teamwork. 
-                </p>
-                <p>
-                With each project, my goal is to utilize my skills to their fullest potential while challenging myself, so that I learn something new every time. I applied my knowledge of React and agile development in a recent project where I worked in a team of 5 to develop a component-based MERN app that provides a fun way for individuals to see how well they know each other.
-                </p>
-                <p>
-                When I’m not project planning or developing, I enjoy drawing, soccer, reading, or finding new technologies I can learn that will make me a better developer.
-                </p>
-                <p>
-                I’m excited to leverage my skills in the future as part of a fast-paced, quality-driven team. Reach out to me to chat about our next project via my contact page located above!
-                </p>
-            </article>
+      <h2 className="title">About Me</h2>
+      <img
+        id="photo"
+        src="../../images/pfp.jpg"
+        alt="placeholder"
+        width="250"
+        height="250"
+      />
+      <p>
+        I am a Web Developer with a background in technical support engineering.
+        I earned a certificate in Full Stack Development from the University of
+        Texas at Austin, where I developed skills in TypeScript, Sass, React.js,
+        MongoDB and more.
+      </p>
+      <p>
+        An innovative problem solver passionate about developing new
+        technologies, with a focus on mobile-first practices and test-driven
+        development. My schooling in 3D modeling and animation have prepared me
+        to approach problems with creativity and teamwork.
+      </p>
+      <p>
+        With each project, my goal is to utilize my skills to their fullest
+        potential while challenging myself, so that I learn something new every
+        time. I applied my knowledge of React and agile development in a recent
+        project where I worked in a team of 5 to develop a component-based MERN
+        app that provides a fun way for individuals to see how well they know
+        each other.
+      </p>
+      <p>
+        When I’m not project planning or developing, I enjoy drawing, soccer,
+        reading, or finding new technologies I can learn that will make me a
+        better developer.
+      </p>
+      <p>
+        I’m excited to leverage my skills in the future as part of a fast-paced,
+        quality-driven team. Reach out to me to chat about our next project via
+        my contact page located above!
+      </p>
+    </article>
   );
 }
 
